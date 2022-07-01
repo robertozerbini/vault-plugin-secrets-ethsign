9bff2ddc89b1e25607ab2017de314cf4d3dbec7a
robertozerbini <zerbini.roberto@gmail.com>
2022-07-01 18:00:29 +0400

    Update accounts_test.go

--- backend/accounts_test.go
+++ backend/accounts_test.go
@@ -189,7 +189,7 @@ func TestAccounts(t *testing.T) {
 	v, _, _ := tx.RawSignatureValues()
 	assert.Equal(true, contains([]*big.Int{big.NewInt(27), big.NewInt(28)}, v))
 
-	sender, _ := types.Sender(types.HomesteadSigner{}, &tx)
+	sender, _ := types.Sender(types.LatestSignerForChainID(12345), &tx)
 	assert.Equal(address1, strings.ToLower(sender.Hex()))
 
 	// sign TX by address without "0x" using EIP155 signer
