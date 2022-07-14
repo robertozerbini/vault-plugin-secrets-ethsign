f2d1ba164bf035885c3a0ae45334f713326588bb
robertozerbini <zerbini.roberto@gmail.com>
2022-07-14 19:15:12 +0400

    Update accounts.go

--- backend/accounts.go
+++ backend/accounts.go
@@ -279,7 +279,14 @@ func (b *backend) signTx(ctx context.Context, req *logical.Request, data *framew
 
 	var tx *types.Transaction
 	
-	accesses  = AccessList{{Address: &toAddress, StorageKeys: []common.Hash{{0}}}}
+	type AccessList []AccessTuple
+	// AccessTuple is the element type of an access list.
+	type AccessTuple struct {
+		Address     common.Address `json:"address"        gencodec:"required"`
+		StorageKeys []common.Hash  `json:"storageKeys"    gencodec:"required"`
+	}
+	
+	var accesses  = AccessList{{Address: &toAddress, StorageKeys: []common.Hash{{0}}}}
 	
 	if rawAddressTo == "" {
 		tx = types.NewTx(&types.DynamicFeeTx{
